a29048077447f5b630b2298f9da0977a14c35c09
gaoxiaobao2021 <m13623642550@163.com>
2023-06-29 22:53:39 +0800

    增加可视化交互窗口 增加回车搜索功能

--- chatbot_graph2.py
+++ chatbot_graph2.py
@@ -1,38 +1,39 @@
 #!/usr/bin/env python3
 # coding: utf-8
-# File: chatbot_graph.py
-# Author: lhy<lhy_in_blcu@126.com,https://huangyong.github.io>
-# Date: 18-10-4
 
 from question_classifier import *
 from question_parser import *
 from answer_search import *
 import ttkbootstrap as tk
 
-'''问答类'''
-
 
 class ChatBotGraph:
+    """问答类"""
+
     def __init__(self):
         self.classifier = QuestionClassifier()
         self.parser = QuestionPaser()
         self.searcher = AnswerSearcher()
 
-    def chat_main(self, sent):
-        answer = '您好，我是小勇医药智能助理，希望可以帮到您。如果没答上来，可联系https://liuhuanyong.github.io/。祝您身体棒棒！'
-        res_classify = self.classifier.classify(sent)
-        print('chat_main sent→', sent)
+    def chat_main(self, question):
+        answer = '抱歉没有找到您想要的答案！'
+        res_classify = self.classifier.classify(question)
+        print('chat_main question→', question)
         if not res_classify:
+            print('chat_main answer1→', answer)
             return answer
         res_sql = self.parser.parser_main(res_classify)
         final_answers = self.searcher.search_main(res_sql)
         if not final_answers:
+            print('chat_main answer2→', answer)
             return answer
         else:
+            print('chat_main answer3→', final_answers)
             return '\n'.join(final_answers)
 
 
-def get_entry_contetn():
+def get_answer(event=None):
+    """显示问答结果"""
     print("e2: ", qa_input.get())
     answer = handler.chat_main(qa_input.get())
     text.delete("0.0", 'end')
@@ -42,15 +43,16 @@ def get_entry_contetn():
 if __name__ == '__main__':
     handler = ChatBotGraph()
     root = tk.Window()
+    root.title('医疗知识图谱智能问答')
     root.geometry('500x550')
     qa_input = tk.Entry(root, width=65, bootstyle='PRIMARY')
     qa_input.grid(row=10, column=1, sticky=tk.W, padx=10, pady=10)
 
     tk.Button(root, width=5, text="提问", bootstyle=('PRIMARY', "outline-toolbutton"),
-              command=get_entry_contetn).grid(row=10, column=1, sticky=tk.W, padx=420, pady=10)
-
+              command=get_answer).grid(row=10, column=1, sticky=tk.W, padx=420, pady=10)
     # 结果展示区
     text = tk.Text(root, width=65, height=25)
     text.grid(row=30, column=1, sticky=tk.W, padx=10, pady=10)
 
+    root.bind('<Return>', get_answer)  # 回车搜索
     root.mainloop()
